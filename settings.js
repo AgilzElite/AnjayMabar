7ace62261ca29b511e779b32da27f7aef628bcda
AgilzElite <113176792+AgilzElite@users.noreply.github.com>
2022-09-26 17:34:00 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "4KEYS 3S" //namabot kalian
-global.ownername= "4K SANZZ" //nama kalian
+global.botname = "Crazy Botz" //namabot kalian
+global.ownername= "Gilzz" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/4KSanzz" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.youtube = "https://youtube.com/c/ZerosMods🅥" //bebas asal jan hapus
+global.myweb2 = "gadaaaaa" //bebas
+global.email = "zanwaragil13@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6281236167286","6281236167286"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281236167286" // nomor wa kalian
-global.ownernomerr = "+6281236167286" //nmr wa kalian
+global.premium = ['6285857537269'] //premium user
+global.owner = ["6285857537269","6285857537269"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285857537269" // nomor wa kalian
+global.ownernomerr = "+6285857537269" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.donasi = "./image/gadaa" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Crazy-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh Gilzz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
